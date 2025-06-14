3b26665acfc8a683d7b43c9d7584863b9a8733dd
adrien <ad.thibault18@gmail.com>
2025-06-14 16:30:09 +0200

    updated css and front

--- my_calendar/static/js/manage.js
+++ my_calendar/static/js/manage.js
@@ -1,10 +1,21 @@
 
 
+function close_manage_popup() {
+    document.getElementById("manage_popup").innerHTML = '';
+}
+
+
 function create_day_event_popup() {
     document.getElementById("manage_popup").innerHTML = `
-        <span>Name : </span><input type="text" value="" id="create_day_event_name">
-        <input type="date" value="" id="create_day_event_date">
-        <button onclick="create_day_event()">Create</button>`;
+        <div class="manage_popup_background">
+            <div class="manage_popup_contents">
+                <button class="manage_popup_close_button" onclick="close_manage_popup()">x</button>
+                <h3>Create Day Event</h3>
+                <span>Name : </span><input type="text" value="" id="create_day_event_name">
+                <input type="date" value="" id="create_day_event_date">
+                <button onclick="create_day_event()">Create</button>
+            </div>
+        </div>`;
 }
 
 
@@ -33,10 +44,16 @@ function create_day_event() {
 
 function create_yearly_event_popup() {
     document.getElementById("manage_popup").innerHTML = `
-        <span>Name : </span><input type="text" value="" id="create_yearly_event_name">
-        <input type="number" min="1" max="31" value="" id="create_yearly_event_day">
-        <input type="number" min="1" max="12" id="create_yearly_event_month">
-        <button onclick="create_yearly_event()">Create</button>`;
+        <div class="manage_popup_background">
+            <div class="manage_popup_contents">
+                <button class="manage_popup_close_button" onclick="close_manage_popup()">x</button>
+                <h3>Create Yearly Event</h3>
+                <span>Name : </span><input type="text" value="" id="create_yearly_event_name">
+                <input type="number" min="1" max="31" value="" id="create_yearly_event_day">
+                <input type="number" min="1" max="12" id="create_yearly_event_month">
+                <button onclick="create_yearly_event()">Create</button>
+            </div>
+        </div>`;
 }
 
 
@@ -62,4 +79,20 @@ function create_yearly_event() {
         document.getElementById("manage_popup").innerHTML = "";
         retrieve_calendar_data();
     })
+}
+
+
+function hide_plus_menu() {
+    document.getElementById("plus_container").innerHTML = `
+        <button onclick="display_plus_menu()" class="plus_button">+</button>
+        `;
+}
+
+
+function display_plus_menu() {
+    document.getElementById("plus_container").innerHTML = `
+        <button onclick="hide_plus_menu()" class="plus_button">-</button>
+        <button onclick="create_day_event_popup()" class="plus_menu_button">+ day event</button>
+        <button onclick="create_yearly_event_popup()" class="plus_menu_button">+ yearly event</button>
+        `;
 }
